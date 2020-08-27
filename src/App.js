97d1202c35e148a0a850cf81ea68e0d7b0e5cbfe
Daniel Cifuentes <danixbox1313@gmail.com>
2020-08-27 18:41:54 -0500

    added login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Login } from './components/Login.js';
+
+import { BrowserRouter as Router, Link, Route, Redirect, Switch } from 'react-router-dom';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isLoggedIn: false };
+
+        localStorage.setItem("dcifuentes", "testPassword");
+        localStorage.setItem("lrodriguez", "password");
+
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    
+
+    handleLogin(username, password) {
+        if (localStorage.getItem(username) == null || localStorage.getItem(username) != password) {
+
+        } else {
+            this.setState({ isLoggedIn: true });
+        }
+
+    }
+
+    render() {
+
+        const PrivateRoute = ({ children, ...rest }) => {
+    
+            return (
+                <Route
+                
+                    {...rest}
+                    render={()=>{return this.state.isLoggedIn ? children : <Redirect to="/"/>}}
+    
+                />
+            );
+        }
+        const LoginView = () => (
+            <Login handleLogin={this.handleLogin} />
+        );
+
+        return (
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
+
+                    <Switch>
+                    <Route exact path="/">
+                      {LoginView}
+                        </Route>
+                        
+                    </Switch>
+                </div>
+            </Router>
+
+        );
+    }
+
+
+
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import React from '../../node_modules/react';
+import Avatar from '../../node_modules/@material-ui/core/Avatar';
+import Button from '../../node_modules/@material-ui/core/Button';
+import CssBaseline from '../../node_modules/@material-ui/core/CssBaseline';
+import FormControl from '../../node_modules/@material-ui/core/FormControl';
+import Input from '../../node_modules/@material-ui/core/Input';
+import InputLabel from '../../node_modules/@material-ui/core/InputLabel';
+import LockIcon from '../../node_modules/@material-ui/icons/LockOutlined';
+import Paper from '../../node_modules/@material-ui/core/Paper';
+import Typography from '../../node_modules/@material-ui/core/Typography';
+import './css/Login.css'
+
+
+export class Login extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <main className="layout">
+                    <Paper className="paper">
+                        <Avatar className="avatar">
+                            <LockIcon />
+                        </Avatar>
+                        <Typography variant="h2">Sign in</Typography>
+                        <form className="form">
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" autoComplete="email" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </FormControl>
+                            <Button
+                                onClick={(event) => {event.preventDefault();this.props.handleLogin(document.getElementById("email").value,document.getElementById("password").value)}}
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className="submit"
+                            >
+                                Sign in
+                            </Button>
+                        </form>
+                    </Paper>
+                </main>
+            </React.Fragment>
+        );
+    }
+
+}
